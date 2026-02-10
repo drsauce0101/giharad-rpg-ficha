1903871bf53fcaccb0ca3d1fe7afd994b7cf3d09
DrSauce0101 <drsandman000@gmail.com>
2026-02-10 17:58:47 -0300

    feat: backend com criação e listagem de personagens funcionando

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from .database import engine
-from .models import SQLModel, Personagem # Importando o modelo do personagem
+from fastapi import FastAPI, Depends # Injeção de dependência da FastAPI
+from sqlmodel import Session, select
+from .database import engine, get_session # Função de conexão
+from .models import SQLModel, Personagem # Importando o modelo de personagem
 
 # Lifespan do Servidor (roda automaticamente quando starta)
 @asynccontextmanager
@@ -14,7 +15,24 @@ async def lifespan(app: FastAPI):
 # Começo da API
 app = FastAPI(lifespan=lifespan)
 
-# Teste no navegador pra ver se ta rodando
+# ROTAS
+
 @app.get("/")
 def home():
-    return {"status": "Online", "mensagem": "Rodando"}
+    return {"status": "Online"}
+
+# ROTA POST: Criação do personagem
+
+@app.post("/personagem/", response_model=Personagem)
+def criar_personagem(personagem: Personagem, session: Session = Depends(get_session)):
+    session.add(personagem)      # Prepara o salvamento
+    session.commit()             # Salva no banco de verdade
+    session.refresh(personagem)  # Atualiza o objeto e pega o ID gerado
+    return personagem
+
+# ROTA GET: Lista tudo
+@app.get("/personagem/", response_model=list[Personagem])
+def listar_personagens(session: Session = Depends(get_session)):
+    statement = select(Personagem)
+    resultados = session.exec(statement).all()
+    return resultados
