d64ed5aabe6add9fe29b3b241d32de1a05668f9f
DrSauce0101 <drsandman000@gmail.com>
2026-02-10 19:26:05 -0300

    feat: layout levemente mais complexo da ficha e função de delete

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from contextlib import asynccontextmanager
 from pathlib import Path
-from fastapi import FastAPI, Depends, Request, Form # Request do Jinga
-from fastapi.templating import Jinja2Templates # Ferramenta de Templates do Jinga
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Depends, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse # <--- Importante pra funcionar o Delete
 from sqlmodel import Session, select
 from .database import engine, get_session
 from .models import SQLModel, Personagem
@@ -13,7 +13,6 @@ async def lifespan(app: FastAPI):
     # Builda as tabelas
     SQLModel.metadata.create_all(engine)
     yield
-    # Ordens de shutdown (por enquanto nenhuma)
 
 # API
 app = FastAPI(lifespan=lifespan)
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / "frontend" / "templates"
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 
-# ROTAS
+# --- ROTAS ---
 
 # ROTA HOME: Retorna HTML com as fichas
 @app.get("/", response_class=HTMLResponse)
@@ -39,7 +38,7 @@ def home(request: Request, session: Session = Depends(get_session)):
         context={"personagens": resultados}
     )
 
-# ROTA POST: Criação do personagem
+# ROTA POST: Criação do personagem (API/Swagger)
 @app.post("/personagem/", response_model=Personagem)
 def criar_personagem(personagem: Personagem, session: Session = Depends(get_session)):
     session.add(personagem)      # Prepara o salvamento
@@ -54,14 +53,14 @@ def listar_personagens_api(session: Session = Depends(get_session)):
     resultados = session.exec(statement).all()
     return resultados
 
-# ROTAS DE FORMULÁRIO
+# --- ROTAS DE FORMULÁRIO (WEB) ---
 
-# GET
+# GET - Página de Cadastro
 @app.get("/novo", response_class=HTMLResponse)
 def pagina_cadastro(request: Request):
     return templates.TemplateResponse(request=request, name="cadastro.html")
 
-# POST
+# POST - Processar Cadastro
 @app.post("/enviar_cadastro")
 def processar_formulario(
     session: Session = Depends(get_session),
@@ -94,7 +93,8 @@ def processar_formulario(
         inteligencia=inteligencia,
         sabedoria=sabedoria,
         carisma=carisma,
-        # Note que pv_atual começa igual ao pv_max
+        
+        # Status
         pv_max=pv_max,
         pv_atual=pv_max,
         pa_max=pa_max,
@@ -109,5 +109,19 @@ def processar_formulario(
     session.add(novo_char)
     session.commit()
 
-    # REDIRECT: Manda o navegador voltar para a Home (Código 303 = See Other)
+    # REDIRECT: Manda o navegador voltar para a Home
+    return RedirectResponse("/", status_code=303)
+
+# --- ROTA DE EXCLUIR ---
+@app.post("/deletar/{char_id}")
+def deletar_personagem(char_id: int, session: Session = Depends(get_session)):
+    # Busco o personagem no banco pelo ID
+    personagem = session.get(Personagem, char_id)
+    
+    if personagem:
+        # Removo o item e confirmo a transação
+        session.delete(personagem)
+        session.commit()
+    
+    # Redireciono pra home pra ver a lista atualizada
     return RedirectResponse("/", status_code=303)
